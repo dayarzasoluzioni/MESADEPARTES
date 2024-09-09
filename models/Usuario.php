6ced39b2c67c3de510d7046b6697747bc7c03ff8
Dario Ayarza <dayarza@soluzioni.pe>
2024-09-09 10:40:20 -0500

    Registrar Usuario

---
+++ models/Usuario.php
@@ -0,0 +1,29 @@
+<?php
+    /* TODO: Defeinición de la clase Usuario que se extiende de la clase Conectar */
+    class Usuario extends Conectar{
+
+        /* TODO: Método para registrar un nuevo usuario en la base de datos */
+        public function registrar_usuario($usu_nomape, $usu_correo, $usu_pass){
+
+            /* TODO: Obtener la conexión a la base de datos utilizando el método de la clase padre */
+            $conectar = parent::conexion();
+            /* TODO: Establecer el juego de caracteres a UTF-8 utilizando el método de la clase padre */
+            parent::set_names();
+            /* TODO: Consulta SQL para insertar un nuevo usuario en la tabla tm_usuario */
+            $sql="INSERT INTO tm_usuario (usu_nomape, usu_correo, usu_pass) 
+                VALUES (?,?,?)";
+
+            /* TODO: Preparar la consulta SQL */
+            $sql=$conectar->prepare($sql);
+            /* TODO: Vincular los valores a los parámetros de la consulta */
+            $sql->bindValue(1,$usu_nomape);
+            $sql->bindValue(2,$usu_correo);
+            $sql->bindValue(3,$usu_pass);
+            /* TODO: Ejecutar la consulta SQL */
+            $sql->execute();
+
+        }
+
+    }
+
+?>
---
+++ view/registro/index.php
@@ -0,0 +1,243 @@
+
+<!doctype html>
+<html lang="es">
+
+    <head>
+
+        <meta charset="utf-8">
+        <title>Registro | Soluzioni Group - Mesa de Partes</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description">
+        <meta content="Themesbrand" name="author">
+
+        <!-- preloader css -->
+        <link rel="stylesheet" href="../../assets/css/preloader.min.css" type="text/css">
+
+        <!-- Bootstrap Css -->
+        <link href="../../assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
+        <!-- Icons Css -->
+        <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css">
+        <!-- App Css-->
+        <link href="../../assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css">
+
+    </head>
+
+    <body>
+
+    <!-- <body data-layout="horizontal"> -->
+        <div class="auth-page">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-xxl-3 col-lg-4 col-md-5">
+                        <div class="auth-full-page-content d-flex p-sm-5 p-4">
+                            <div class="w-100">
+                                <div class="d-flex flex-column h-100">
+                                    <div class="mb-4 mb-md-5 text-center">
+                                        <a href="index.html" class="d-block auth-logo">
+                                            <img src="../../assets/image/soluzionilogo.png" alt="" height="28"> <span class="logo-txt">Soluzioni Group</span>
+                                        </a>
+                                    </div>
+                                    <div class="auth-content my-auto">
+                                        <div class="text-center">
+                                            <h5 class="mb-0">Registrar Cuenta</h5>
+                                            <p class="text-muted mt-2">Registre sus datos en el portal</p>
+                                        </div>
+                                        <form class="needs-validation custom-form mt-4 pt-2" novalidate="" action="index.html">
+                                            <div class="mb-3">
+                                                <label for="useremail" class="form-label">Correo Electrónico</label>
+                                                <input type="email" class="form-control" id="useremail" placeholder="Ingrese su Correo Electrónico" required="">  
+                                                <div class="invalid-feedback">
+                                                    Por favor ingrese su Correo Electrónico
+                                                </div>      
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="username" class="form-label">Nombres y Apellidos</label>
+                                                <input type="text" class="form-control" id="username" placeholder="Ingrese sus Nombres y Apellidos" required="">
+                                                <div class="invalid-feedback">
+                                                    Por favor ingrese sus Nombres y Apellidos
+                                                </div>  
+                                            </div>
+                    
+                                            <div class="mb-3">
+                                                <label for="userpassword" class="form-label">Contraseña</label>
+                                                <input type="password" class="form-control" id="userpassword" placeholder="Ingrese su Contraseña" required="">
+                                                <div class="invalid-feedback">
+                                                    Por favor ingrese su Contraseña
+                                                </div>       
+                                            </div>
+
+                                            <div class="mb-3">
+                                                <label for="userpassword" class="form-label">Confirmar Contraseña</label>
+                                                <input type="password" class="form-control" id="userpassword" placeholder="Confirme su Contraseña" required="">
+                                                <div class="invalid-feedback">
+                                                    Por favor confirme su Contraseña
+                                                </div>       
+                                            </div>
+
+                                            <div class="mb-4">
+                                                <p class="mb-0">Al registrarse acepta los <a href="#" class="text-primary"><strong>Términos y Condiciones</strong></a></p>
+                                            </div>
+                                            <div class="mb-3">
+                                                <button class="btn btn-primary w-100 waves-effect waves-light" type="submit">Registrarse</button>
+                                            </div>
+                                        </form>
+
+                                        <!-- <div class="mt-4 pt-2 text-center">
+                                            <div class="signin-other-title">
+                                                <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign up using -</h5>
+                                            </div>
+
+                                            <ul class="list-inline mb-0">
+                                                <li class="list-inline-item">
+                                                    <a href="javascript:void()" class="social-list-item bg-primary text-white border-primary">
+                                                        <i class="mdi mdi-facebook"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a href="javascript:void()" class="social-list-item bg-info text-white border-info">
+                                                        <i class="mdi mdi-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a href="javascript:void()" class="social-list-item bg-danger text-white border-danger">
+                                                        <i class="mdi mdi-google"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div> -->
+
+                                        <div class="mt-5 text-center">
+                                            <p class="text-muted mb-0">¿ Ya tienes una cuenta ? <a href="../../index.php" class="text-primary fw-semibold"> Acceder </a> </p>
+                                        </div>
+                                    </div>
+                                    <div class="mt-4 mt-md-5 text-center">
+                                        <p class="mb-0">© <script>document.write(new Date().getFullYear())</script> Soluzioni Group <i class="mdi mdi-heart text-danger"></i> Todos los derechos reservados</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end auth full page content -->
+                    </div>
+                    <!-- end col -->
+                    <div class="col-xxl-9 col-lg-8 col-md-7">
+                        <div class="auth-bg pt-md-5 p-4 d-flex">
+                            <div class="bg-overlay bg-primary"></div>
+                            <ul class="bg-bubbles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                            <!-- end bubble effect -->
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-xl-7">
+                                    <div class="p-0 p-sm-4 px-xl-0">
+                                        <div id="reviewcarouselIndicators" class="carousel slide" data-bs-ride="carousel">
+                                            <div class="carousel-indicators carousel-indicators-rounded justify-content-start ms-0 mb-0">
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                <button type="button" data-bs-target="#reviewcarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                            </div>
+                                            <!-- end carouselIndicators -->
+                                            <div class="carousel-inner">
+                                                <div class="carousel-item active">
+                                                    <div class="testi-contain text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“Excelente empresa y 
+                                                            sus empleados siempre respetuosos. Quiero contrato de por vida”
+                                                        </h4>
+                                                        <div class="mt-4 pt-3 pb-5">
+                                                            <div class="d-flex align-items-start">
+                                                                <div class="flex-shrink-0">
+                                                                    <img src="../../assets/picture/avatar-1-1.jpg" class="avatar-md img-fluid rounded-circle" alt="...">
+                                                                </div>
+                                                                <div class="flex-grow-1 ms-3 mb-4">
+                                                                    <h5 class="font-size-18 text-white">Superior Monzon
+                                                                    </h5>
+                                                                    <p class="mb-0 text-white-50">Oficial PNP</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="carousel-item">
+                                                    <div class="testi-contain text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“Desde que dan desayuno en la mañana 
+                                                            siempre llego temprano.”</h4>
+                                                        <div class="mt-4 pt-3 pb-5">
+                                                            <div class="d-flex align-items-start">
+                                                                <div class="flex-shrink-0">
+                                                                    <img src="../../assets/picture/avatar-2-1.jpg" class="avatar-md img-fluid rounded-circle" alt="...">
+                                                                </div>
+                                                                <div class="flex-grow-1 ms-3 mb-4">
+                                                                    <h5 class="font-size-18 text-white">Fátima Salas
+                                                                    </h5>
+                                                                    <p class="mb-0 text-white-50">Administradora</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="carousel-item">
+                                                    <div class="testi-contain text-white">
+                                                        <i class="bx bxs-quote-alt-left text-success display-6"></i>
+
+                                                        <h4 class="mt-4 fw-medium lh-base text-white">“Su personal siempre está dispuesto
+                                                            a apoyarnos.”</h4>
+                                                        <div class="mt-4 pt-3 pb-5">
+                                                            <div class="d-flex align-items-start">
+                                                                <img src="../../assets/picture/avatar-3-1.jpg" class="avatar-md img-fluid rounded-circle" alt="...">
+                                                                <div class="flex-1 ms-3 mb-4">
+                                                                    <h5 class="font-size-18 text-white">Liliana Sanchez</h5>
+                                                                    <p class="mb-0 text-white-50">ISOC
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!-- end carousel-inner -->
+                                        </div>
+                                        <!-- end review carousel -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end col -->
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container fluid -->
+        </div>
+
+
+        <!-- JAVASCRIPT -->
+        <script src="../../assets/js/jquery.min.js"></script>
+        <script src="../../assets/js/bootstrap.bundle.min.js"></script>
+        <script src="../../assets/js/metisMenu.min.js"></script>
+        <script src="../../assets/js/simplebar.min.js"></script>
+        <script src="../../assets/js/waves.min.js"></script>
+        <script src="../../assets/js/feather.min.js"></script>
+        <!-- pace js -->
+        <script src="../../assets/js/pace.min.js"></script>
+
+        <!-- validation init -->
+        <script src="../../assets/js/validation.init.js"></script>
+
+    </body>
+
+</html>
