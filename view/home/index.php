ee81593847a1190be53fd8a04cff0390bc08d00e
Dario Ayarza <dayarza@soluzioni.pe>
2024-09-12 17:29:58 -0500

    inicio de maquetacion de la pagina principal

--- view/home/index.php
+++ view/home/index.php
@@ -1,1 +1,58 @@
-<h1>Acceso al Sistema Correcto</h1>
+<!doctype html>
+<html lang="es">
+
+    <head>
+        
+        <title>Página de Inicio | Soluzioni Capital - Mesa de Partes</title>
+        <?php require_once("../html/head.php") ?>
+        
+    </head>
+
+    <body>
+
+        <div id="layout-wrapper">
+            
+        <?php require_once("../html/header.php") ?>
+
+            <?php require_once("../html/menu.php") ?>
+
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                    <h4 class="mb-sm-0 font-size-18">Starter Page</h4>
+
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Pages</a></li>
+                                            <li class="breadcrumb-item active">Starter Page</li>
+                                        </ol>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <?php require_once("../html/footer.php") ?>
+
+            </div>
+
+
+        </div>
+
+        <?php require_once("../html/sidebar.php") ?>
+
+        <div class="rightbar-overlay"></div>
+
+        <?php require_once("../html/js.php") ?>
+
+    </body>
+</html>
